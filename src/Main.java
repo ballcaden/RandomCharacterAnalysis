c932a483cd47769c11b8042021ce085bb698cbd3
ballcaden <ballcaden@gmail.com>
2023-10-17 09:42:57 -0400

    init

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        boolean cont = true;
+        String contYN;
+        String word;
+        int random = 0;
+
+        System.out.println("Enter a word/phrase: ");
+        word = scan.nextLine();
+
+        do {
+            if(word.length() > 1) {
+               random = (int)(Math.random()*word.length()) ;
+                word = word.substring(0, random) + word.substring(random +1);
+                System.out.println("Your new word/phrase is: " + word);
+                if(word.length() > 1) {
+                    System.out.println("Again?[Y/N]");
+                    contYN = scan.nextLine();
+                    if(contYN.equalsIgnoreCase("N")) {
+                        cont = false;
+                    }
+                } else {
+                    System.out.println("Your word/phrase can't be smaller.");
+                    cont = false;
+                }
+            } else {
+                System.out.println("Your word/phrase is too small to continue");
+                scan.nextLine();
+            }
+        } while (cont);
+    }
+}
